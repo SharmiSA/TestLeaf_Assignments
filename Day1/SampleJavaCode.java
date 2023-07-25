e2033fa79789d363935c467929a145de4aa719ee
SharmiSA <97806523+SharmiSA@users.noreply.github.com>
2023-07-25 23:32:30 +0530

    Add files via upload

---
+++ Day1/SampleJavaCode.java
@@ -0,0 +1,16 @@
+package week1.Day1;
+
+public class SampleJavaCode 
+{
+
+	public static void main(String[] args) //Main method is like a function
+	{
+		System.out.println("Hello Selenium Java");
+		
+		
+	
+	}
+	
+	
+
+}
---
+++ Day1/Variables.java
@@ -0,0 +1,11 @@
+package week1.Day1;
+
+public class Variables 
+{
+
+	public static void main(String[] args) //Main method is like a function
+	{
+		System.out.println("Java Variables");
+	}
+
+}
